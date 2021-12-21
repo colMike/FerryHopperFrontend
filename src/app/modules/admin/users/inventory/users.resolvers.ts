e217d318ad5b61028759165d316f0d835a7b97ac
Eugene <eugenegregs@gmail.com>
2021-12-21 16:17:33 +0300

    After Moving One Directory Back

---
+++ src/app/modules/admin/users/inventory/users.resolvers.ts
@@ -0,0 +1,172 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { InventoryService } from 'app/modules/admin/customers/inventory/inventory.service';
+import {
+    CustomerObject,
+    InventoryPagination,
+    InventoryProduct
+} from 'app/modules/admin/customers/inventory/inventory.types';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class InventoryBrandsResolver implements Resolve<any>
+{
+    /**
+     * Constructor
+     */
+    constructor(private _inventoryService: InventoryService)
+    {
+    }
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+        return undefined;
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Resolver
+     *
+     * @param route
+     * @param state
+     */
+
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class InventoryCategoriesResolver implements Resolve<any>
+{
+    /**
+     * Constructor
+     */
+    constructor(private _inventoryService: InventoryService)
+    {
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return null;
+    }
+
+
+
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class InventoryProductResolver implements Resolve<any>
+{
+    /**
+     * Constructor
+     */
+    constructor(
+        private _inventoryService: InventoryService,
+        private _router: Router
+    )
+    {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Resolver
+     *
+     * @param route
+     * @param state
+     */
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<InventoryProduct>
+    {
+        return this._inventoryService.getProductById(route.paramMap.get('id'))
+                   .pipe(
+                       // Error here means the requested product is not available
+                       catchError((error) => {
+
+                           // Log the error
+                           console.error(error);
+
+                           // Get the parent url
+                           const parentUrl = state.url.split('/').slice(0, -1).join('/');
+
+                           // Navigate to there
+                           this._router.navigateByUrl(parentUrl);
+
+                           // Throw an error
+                           return throwError(error);
+                       })
+                   );
+    }
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class InventoryProductsResolver implements Resolve<any>
+{
+    /**
+     * Constructor
+     */
+    constructor(private _inventoryService: InventoryService)
+    {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Resolver
+     *
+     * @param route
+     * @param state
+     */
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<{ pagination: InventoryPagination; customers: CustomerObject[] }>
+    {
+        return this._inventoryService.getProducts();
+    }
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class InventoryTagsResolver implements Resolve<any>
+{
+    /**
+     * Constructor
+     */
+    constructor(private _inventoryService: InventoryService)
+    {
+    }
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+        return undefined;
+    }
+
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class InventoryVendorsResolver implements Resolve<any>
+{
+    /**
+     * Constructor
+     */
+    constructor(private _inventoryService: InventoryService)
+    {
+    }
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+        return undefined;
+    }
+
+}
---
+++ src/app/modules/admin/users/portalUser.routing.ts
@@ -0,0 +1,29 @@
+import { Route } from '@angular/router';
+import { InventoryComponent } from 'app/modules/admin/customers/inventory/inventory.component';
+import { InventoryListComponent } from 'app/modules/admin/customers/inventory/list/inventory.component';
+import { InventoryBrandsResolver, InventoryCategoriesResolver, InventoryProductsResolver, InventoryTagsResolver, InventoryVendorsResolver } from 'app/modules/admin/customers/inventory/inventory.resolvers';
+
+export const customerRoutes: Route[] = [
+    {
+        path      : '',
+        pathMatch : 'full',
+        redirectTo: 'inventory'
+    },
+    {
+        path     : 'inventory',
+        component: InventoryComponent,
+        children : [
+            {
+                path     : '',
+                component: InventoryListComponent,
+                resolve  : {
+                    brands    : InventoryBrandsResolver,
+                    categories: InventoryCategoriesResolver,
+                    products  : InventoryProductsResolver,
+                    tags      : InventoryTagsResolver,
+                    vendors   : InventoryVendorsResolver
+                }
+            }
+        ]
+    }
+];
