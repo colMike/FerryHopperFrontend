18b4a1ec7994d5df4f6d0728b5c9692aeb9bc586
Eugene <eugenegregs@gmail.com>
2021-12-21 05:55:40 +0300

    Start of Ferryhopper Project

---
+++ src/app/modules/admin/apps/users/portal-users.routing.ts
@@ -0,0 +1,29 @@
+import { Route } from '@angular/router';
+import { InventoryComponent } from 'app/modules/admin/apps/users/inventory/inventory.component';
+import { InventoryListComponent } from 'app/modules/admin/apps/users/inventory/list/inventory.component';
+import { InventoryBrandsResolver, InventoryCategoriesResolver, InventoryProductsResolver, InventoryTagsResolver, InventoryVendorsResolver } from 'app/modules/admin/apps/users/inventory/inventory.resolvers';
+
+export const portalUserRoutes: Route[] = [
+    {
+        path      : '',
+        pathMatch : 'full',
+        redirectTo: 'inventory'
+    },
+    {
+        path     : 'inventory',
+        component: InventoryComponent,
+        children : [
+            {
+                path     : '',
+                component: InventoryListComponent,
+                resolve  : {
+                    brands    : InventoryBrandsResolver,
+                    categories: InventoryCategoriesResolver,
+                    products  : InventoryProductsResolver,
+                    tags      : InventoryTagsResolver,
+                    vendors   : InventoryVendorsResolver
+                }
+            }
+        ]
+    }
+];
---
+++ src/app/modules/customer/customers.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import {Route, RouterModule} from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { SharedModule } from 'app/shared/shared.module';
+import {ListCustomersComponent} from './listCustomers/list-customers.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+const customerRoutes: Route[] = [
+    {
+        path     : '',
+        component: ListCustomersComponent
+    }
+];
+
+@NgModule({
+    declarations: [
+        ListCustomersComponent
+    ],
+    imports     : [
+        RouterModule.forChild(customerRoutes),
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatMenuModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatRippleModule,
+        MatSortModule,
+        MatSelectModule,
+        MatSlideToggleModule,
+        MatTooltipModule,
+    ]
+})
+export class CustomersModule
+{
+}
